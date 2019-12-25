0b77a5da40ec638d5b74f738b129c3d90480a542
Jeff Lee <jeffomatic@gmail.com>
2019-12-25 00:37:00 -0800

    day17b: finish

--- day17b/src/main.rs
+++ day17b/src/main.rs
@@ -1,6 +1,6 @@
 use intcode;
-use std::collections::HashSet;
 use std::io::{self, Read};
+use std::mem;
 
 #[derive(Copy, Clone, Debug)]
 enum Dir {
@@ -21,56 +21,50 @@ impl Dir {
         }
     }
 
-    fn to_char(&self) -> char {
+    fn left(&self) -> Dir {
         match self {
-            Dir::N => '^',
-            Dir::S => 'v',
-            Dir::E => '>',
-            Dir::W => '<',
+            Dir::N => Dir::W,
+            Dir::S => Dir::E,
+            Dir::E => Dir::N,
+            Dir::W => Dir::S,
         }
+    }
+
+    fn right(&self) -> Dir {
+        match self {
+            Dir::N => Dir::E,
+            Dir::S => Dir::W,
+            Dir::E => Dir::S,
+            Dir::W => Dir::N,
+        }
+    }
+
+    fn move_from(&self, p: (i64, i64)) -> (i64, i64) {
+        match self {
+            Dir::N => (p.0, p.1 - 1),
+            Dir::S => (p.0, p.1 + 1),
+            Dir::E => (p.0 + 1, p.1),
+            Dir::W => (p.0 - 1, p.1),
+        }
+    }
+
+    fn turn(from: Self, to: Self) -> Option<String> {
+        if mem::discriminant(&from.left()) == mem::discriminant(&to) {
+            return Some("L".to_string());
+        }
+
+        if mem::discriminant(&from.right()) == mem::discriminant(&to) {
+            return Some("R".to_string());
+        }
+
+        None
     }
 }
 
+#[derive(Clone, Copy, Debug)]
 struct BotState {
-    pos: (usize, usize),
+    pos: (i64, i64),
     orientation: Dir,
-}
-
-fn get_input() -> String {
-    let mut input = String::new();
-    io::stdin().lock().read_to_string(&mut input).unwrap();
-    input.trim().to_string()
-}
-
-fn render(rows: &Vec<Vec<bool>>, bot: &BotState) {
-    for (i, row) in rows.iter().enumerate() {
-        for (j, col) in row.iter().enumerate() {
-            if (j, i) == bot.pos {
-                print!("{}", bot.orientation.to_char());
-                continue;
-            }
-            print!("{}", if *col { '#' } else { '.' });
-        }
-        print!("\n");
-    }
-}
-
-fn get_intersections(rows: &Vec<Vec<bool>>) -> HashSet<(usize, usize)> {
-    let h = rows.len();
-    let w = rows[0].len();
-    let mut result = HashSet::new();
-
-    // can't have intersections at the top or bottom row
-    for i in 1..(h - 1) {
-        // can't have intersections at the left or right columns
-        for j in 1..(w - 1) {
-            if rows[i][j] && rows[i - 1][j] && rows[i + 1][j] && rows[i][j - 1] && rows[i][j + 1] {
-                result.insert((j, i));
-            }
-        }
-    }
-
-    return result;
 }
 
 // returns a tuple of:
@@ -113,16 +107,90 @@ fn parse_output(output: &Vec<i64>) -> (Vec<Vec<bool>>, BotState) {
     (rows, bot.unwrap())
 }
 
+fn on_scaffold(map: &Vec<Vec<bool>>, p: (i64, i64)) -> bool {
+    let h = map.len() as i64;
+    let w = map[0].len() as i64;
+    0 <= p.0 && p.0 < w && 0 <= p.1 && p.1 < h && map[p.1 as usize][p.0 as usize]
+}
+
+fn scaffold_path(map: &Vec<Vec<bool>>, bot: BotState) -> Vec<BotState> {
+    let mut path = vec![bot];
+    let mut bot = bot.clone();
+
+    'next_step: loop {
+        let guesses = [
+            bot.orientation,
+            bot.orientation.left(),
+            bot.orientation.right(),
+        ];
+
+        for d in guesses.iter() {
+            let p = d.move_from(bot.pos);
+            if on_scaffold(&map, p) {
+                bot = BotState {
+                    pos: p,
+                    orientation: *d,
+                };
+                path.push(bot);
+                continue 'next_step;
+            }
+        }
+
+        // If we get here, we've exhausted our guesses, and the path is complete.
+        return path;
+    }
+}
+
+fn path_to_commands(path: &Vec<BotState>) -> Vec<String> {
+    let mut commands = Vec::new();
+    let mut forward = 1;
+
+    for i in 0..path.len() - 1 {
+        match Dir::turn(path[i].orientation, path[i + 1].orientation) {
+            None => forward += 1,
+            Some(s) => {
+                if forward > 0 && i > 0 {
+                    commands.push(forward.to_string());
+                    forward = 1;
+                }
+                commands.push(s);
+            }
+        }
+    }
+
+    if forward > 0 {
+        commands.push(forward.to_string());
+    }
+
+    commands
+}
+
+fn get_input() -> String {
+    let mut input = String::new();
+    io::stdin().lock().read_to_string(&mut input).unwrap();
+    input.trim().to_string()
+}
+
 fn main() {
-    let program: Vec<i64> = get_input().split(",").map(|s| s.parse().unwrap()).collect();
+    let mut program: Vec<i64> = get_input().split(",").map(|s| s.parse().unwrap()).collect();
     let result = intcode::Computer::new(&program).run(&Vec::new());
-    let parsed = parse_output(&result.output);
-    render(&parsed.0, &parsed.1);
-    println!(
-        "{:?}",
-        get_intersections(&parsed.0)
-            .iter()
-            .map(|p| p.0 * p.1)
-            .fold(0, |acc, v| acc + v)
-    );
+    let (map, bot) = parse_output(&result.output);
+    let path = scaffold_path(&map, bot);
+    let commands = path_to_commands(&path);
+    println!("{}", commands.join(","));
+
+    // Set program to manual control
+    program[0] = 2;
+
+    let input_str = "A,B,B,A,B,C,A,C,B,C
+L,4,L,6,L,8,L,12
+L,8,R,12,L,12
+R,12,L,6,L,6,L,8
+n
+";
+    let input: Vec<i64> = input_str.chars().map(|c| c as i64).collect();
+
+    // Run program with routine
+    let result = intcode::Computer::new(&program).run(&input);
+    println!("{}", result.output.last().unwrap());
 }
